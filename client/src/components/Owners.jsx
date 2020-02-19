05287143de5cfff6c627da430d6d25edf15bd9a0
John Jacobs <john.a.jacobs1973@gmail.com>
2020-02-19 15:23:26 -0500

    owner cards

--- client/src/components/Owners.jsx
+++ client/src/components/Owners.jsx
@@ -2,13 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 
 export default function Owners() {
     const [owners, setOwners] = useState([]);
     useEffect(() => {
         async function fetchData() {
-        const res = await axios.get('/api/user')
-        setOwners(res.data)
+            const res = await axios.get('/api/user')
+            setOwners(res.data)
         }
         fetchData()
     }, [])
@@ -16,12 +17,18 @@ export default function Owners() {
         <div>
             <h1>Owners:</h1>
             {owners.map(owner => (
-                <Link to={{
-                    pathname: `/${owner._id}`,
-                    owner: {
-                        name: owner.name
-                    }
-                }} key={owner._id}><h2>{owner.name}</h2></Link>
+                <div className="ownerCard">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={owner.image} />
+                        <Link to={{
+                            pathname: `/${owner._id}`,
+                            owner: {
+                                name: owner.name
+                            }
+                        }} key={owner._id}><h2>{owner.name}</h2></Link>
+                        <Card.Text>Location: {owner.location}</Card.Text>
+                    </Card>
+                </div>
             ))}
         </div>
     )
