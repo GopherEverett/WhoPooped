5848a4ec8d5a45d18cca16a7ebbed277f721efba
John Jacobs <john.a.jacobs1973@gmail.com>
2020-09-15 15:46:19 -0400

    removed okta

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Owners from './components/Owners'
 import Owner from './components/Owner'
 import NewOwnerForm from './components/NewOwnerForm'
@@ -11,7 +11,6 @@ import { Navbar, Nav } from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Navbar bg="light" variant="light
         " className="justify-content-between">
           <Navbar.Brand>Who Pooped?</Navbar.Brand>
@@ -25,7 +24,6 @@ function App() {
           <Route exact path='/:ownerId/newDog' component={NewDogForm} />
           <Route exact path='/:ownerId' component={Owner} />
         </Switch>
-      </BrowserRouter>
     </div>
   );
 }
